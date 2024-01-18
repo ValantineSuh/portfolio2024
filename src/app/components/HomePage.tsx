a0dffbcabddb4705fd0f986d5fc8ec2b944fc9e2
ESUM BRANDON <esumbrandon074@gmail.com>
2024-01-18 10:52:06 +0100

    Modifications on home page

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -5,15 +5,17 @@ import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
 
 const HomePage = () => {
   return (
-    <div className='h-screen w-full bg-gradient-to-b from-black to-gray-800'>
+    <div className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
         
-        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 text-white md:flex-row'>
-            <div>
-                <h2>
-                Lorem ipsum dolor sit  Aperiam voluptatibus autem maxime?
+        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+            <div className='flex flex-col justify-center h-full mx-20'>
+                <h2 className='text-4xl sm:text-7xl font-bold text-white' >
+                I am a full  Developer
                 </h2>
-                <p>
- natus  deleniti. Debitis nesciunt rerum non aperiam magni, iusto dolorum!
+                <p className='text-gray-500 py-4 max-w-md'>
+                I have 5 years of experiece building, designing and maintaining software appllications
+                Currently, I work on web and mobile applications using the technologies
+                NextJs (React JS), Tailwind css, TypeScript, Flutter, GraphQL, firebase and mongoDB
                 </p>
                 <div>
                     <button>
@@ -23,9 +25,10 @@ const HomePage = () => {
                         </span>
                     </button>
                 </div>
-                <Image src={myImages} alt='meh' className='rounded-2xl mx-auto w-2/3 md:w-full' width={400} height={600}/>
             </div>
+            <Image src={myImages} alt='meh' className='rounded-2xl mx-auto w-2/3 md:w-full' width={200} height={600}/>
         </div>
+       
     </div>
   )
 }
