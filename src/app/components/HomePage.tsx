96adf7e77f45069d4863fbb332024552d39de4b1
ESUM BRANDON <esumbrandon074@gmail.com>
2024-01-18 11:02:10 +0100

    Animation made on portfolio button and BG-gradient added to it

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -8,7 +8,7 @@ const HomePage = () => {
     <div className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
         
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-            <div className='flex flex-col justify-center h-full mx-20'>
+            <div className='flex flex-col justify-center h-full '>
                 <h2 className='text-4xl sm:text-7xl font-bold text-white' >
                 I am a full  Developer
                 </h2>
@@ -18,9 +18,10 @@ const HomePage = () => {
                 NextJs (React JS), Tailwind css, TypeScript, Flutter, GraphQL, firebase and mongoDB
                 </p>
                 <div>
-                    <button>
+                    <button className='text-white w-fit px-6 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500
+                    to-blue-500 cursor-pointer group'> {/**the group attribute is to make sure you group same props to another tag */}
                         Portfolio
-                        <span> 
+                        <span className='group-hover:rotate-90 duration-300'> 
                             <MdOutlineKeyboardArrowRight />
                         </span>
                     </button>
