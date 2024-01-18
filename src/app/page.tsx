975a1f26b10693e0101f4d920106ea41c35d11d8
ESUM BRANDON <esumbrandon074@gmail.com>
2024-01-18 23:49:12 +0100

    Done with the experience component, need modifications

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import HomePage from './components/HomePage'
 import Socials from './components/Socials'
 import About from './components/About'
 import Portfolio from './components/Portfolio'
+import Experience from './components/Experience'
+import ContactMe from './components/ContactMe'
 
 
 export default function Home() {
@@ -14,7 +16,11 @@ export default function Home() {
   <HomePage />
   <About />
   <Portfolio />
+    {/**Experience section hard coded and i didn't wanna create any component for it, will do so soon */}
+    {/**Changed my mind */}
 
+    <Experience />
+    <ContactMe />
   <Socials />
 </div>
 
