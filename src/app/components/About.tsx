595584ee31e9d05920fd5414516a905864880357
ESUM BRANDON <esumbrandon074@gmail.com>
2024-01-20 20:06:51 +0100

    Updates on line breaks

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -11,7 +11,7 @@ const About = () => {
     </div>
     <p className='text-xl mt-20'>
    Hey there, it's Esum Brandon.O😎, I am an experienced Software engineer with a degree in computer software engineering at the University of Buea, Cameroon.
-  I've contributed to lots of projects using the named frameworks in the <strong> Experience </strong> Section
+  I've contributed to lots of projects using the named frameworks in the <strong>  Experience </strong> Section
 
 
 
@@ -20,10 +20,10 @@ const About = () => {
       <p className='text-xl'>
       Skilled software engineer with 4 years of experience in the industry. Highly adept at handling various responsibilities by
  prioritizing my tasks, establishing clear deadlines and finding creative solutions to eliminate obstacles. My experience has
-  equipped me with the necessary tools to success this position:
-      * Mobile App dev.
-      * Web App Dev.
-      * Desktop App dev.
+  equipped me with the necessary tools to success this position: <br />
+      * Mobile App dev. <br/>
+      * Web App Dev. <br />
+      * Desktop App dev <br />
       </p>
   </div>
  </div>
